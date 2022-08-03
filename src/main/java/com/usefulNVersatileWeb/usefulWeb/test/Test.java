0cccc9234b955a74340e4835914154633f64956f
jeonghoon <vvv6737@naver.com>
2022-08-03 13:01:28 +0900

    update

--- src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
@@ -1,9 +1,13 @@
 package com.usefulNVersatileWeb.usefulWeb.test;
 
+import com.usefulNVersatileWeb.usefulWeb.util.StringUtil;
+
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class Test{
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String a = StringUtil.sha256("");
+        System.out.println(a.length());
     }
 }
