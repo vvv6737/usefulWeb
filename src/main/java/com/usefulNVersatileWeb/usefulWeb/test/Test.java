d1b3aa8cabbde4ede6588720f4518333b84d257a
jeonghoon <vvv6737@naver.com>
2022-08-16 20:10:21 +0900

    update

--- src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Test{
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        String a = StringUtil.sha256("");
+        String a = StringUtil.sha256("5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5");
         System.out.println(a);
     }
 }
