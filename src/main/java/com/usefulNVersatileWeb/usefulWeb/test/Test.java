d12f49d0bb07c292fa704e0c69f071a82ea16939
vvv6737 <vvv6737@naver.com>
2022-09-07 23:33:43 +0900

    update

--- src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
@@ -1,14 +1,19 @@
 package com.usefulNVersatileWeb.usefulWeb.test;
 
 import com.usefulNVersatileWeb.usefulWeb.util.StringUtil;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class Test{
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        String a = StringUtil.sha256("");
+    public static void main(String[] args) throws JSONException {
+
+        String a = "'{\"0\":{\"test1\":123,\"fff\":\"567\"},\"1\":{\"test1\":456,\"fff\":\"fsdakfs\"}}'";
+        JSONObject jObj = new JSONObject(a);
+
         System.out.println(a);
     }
 }
