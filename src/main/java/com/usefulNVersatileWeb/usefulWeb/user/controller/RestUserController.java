016247a3762795dd833f882c6d7063dd850c4df6
vvv6737 <vvv6737@naver.com>
2022-09-23 23:27:45 +0900

    update

--- src/main/java/com/usefulNVersatileWeb/usefulWeb/user/controller/RestUserController.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/user/controller/RestUserController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.security.NoSuchAlgorithmException;
 
