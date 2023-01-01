c8615ef2cba9cfc56e2cfbb8c2754efe31bab64a
vvv6737 <vvv6737@naver.com>
2023-01-01 20:32:33 +0900

    update

--- src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
@@ -1,9 +1,7 @@
 package com.usefulNVersatileWeb.usefulWeb.test;
 
-import org.openqa.selenium.devtools.v85.input.Input;
-
 public class Test{
     public static void main(String[] args) {
-        Input a = null;
+
     }
 }
