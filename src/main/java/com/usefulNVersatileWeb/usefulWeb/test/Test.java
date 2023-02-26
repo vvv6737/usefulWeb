c4aa8f585abd5377ae9b056991cf96eed08b5518
jeonghoon <vvv6737@naver.com>
2023-02-26 22:21:38 +0900

    update

--- src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/test/Test.java
@@ -1,7 +1,22 @@
 package com.usefulNVersatileWeb.usefulWeb.test;
 
+import com.usefulNVersatileWeb.usefulWeb.util.StringUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test{
     public static void main(String[] args) {
-        System.out.println("test");
+        Map<String, Object> tMap = new HashMap<>();
+
+        tMap.put("아아","이잉");
+        tMap.put("어어","어엉");
+        tMap.put("우우","우웅");
+
+        System.out.println("tMap : " + tMap);
+
+        String str = StringUtil.formatQueryParams(tMap) ;
+
+        System.out.println("str : " + str);
     }
 }
--- src/main/java/com/usefulNVersatileWeb/usefulWeb/util/StringUtil.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/util/StringUtil.java
@@ -2,6 +2,9 @@ package com.usefulNVersatileWeb.usefulWeb.util;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class StringUtil {
@@ -40,7 +43,10 @@ public class StringUtil {
     }
 
 //    map을 queryString으로 변환한다.
-    public String formatQueryParams(Map<String, Object> params) {
-        return null;
+    public static String formatQueryParams(Map<String, Object> params) {
+        return params.entrySet().stream().map(p -> p.getKey() + "=" + p.getValue())
+                .reduce((p1, p2) -> p1 + "&" + p2)
+                .map(s -> "?" + s)
+                .orElse("");
     }
 }
