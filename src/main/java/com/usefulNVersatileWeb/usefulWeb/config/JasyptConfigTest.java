c061832ddd75bb577851eff664abdcb9919a3c44
jeonghoon <vvv6737@naver.com>
2023-04-19 19:39:29 +0900

    update

--- src/main/java/com/usefulNVersatileWeb/usefulWeb/config/JasyptConfigTest.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/config/JasyptConfigTest.java
@@ -1,7 +1,41 @@
 package com.usefulNVersatileWeb.usefulWeb.config;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource("classpath:application.properties")
 public class JasyptConfigTest {
     public static void main(String[] args) {
 
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String password = "1234";
+
+        String encryptUrl = jasyptEncrypt(url);
+        String encryptUsername = jasyptEncrypt(username);
+        String encryptPassword = jasyptEncrypt(password);
+
+        System.out.println("encryptUrl : " + encryptUrl);
+        System.out.println("encryptUsername : " + encryptUsername);
+        System.out.println("encryptPassword" + encryptPassword);
+
+//        Assertions.assertThat(url).isEqualTo(jasyptDecryt(encryptUrl));
+
+    }
+
+    private static String jasyptEncrypt(String input) {
+        String key = "5678";
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.encrypt(input);
+    }
+
+    private static String jasyptDecryt(String input){
+        String key = "5678";
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.decrypt(input);
     }
 }
