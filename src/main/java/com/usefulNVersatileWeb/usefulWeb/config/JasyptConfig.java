3076dc99dde966bb194672c962cca83e5fd74466
jeonghoon <vvv6737@naver.com>
2023-04-24 20:03:06 +0900

    prop암호화 수정

--- src/main/java/com/usefulNVersatileWeb/usefulWeb/config/JasyptConfig.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/config/JasyptConfig.java
@@ -3,6 +3,7 @@ package com.usefulNVersatileWeb.usefulWeb.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,8 @@ import org.springframework.stereotype.Component;
 @Configuration
 public class JasyptConfig {
 
-    private final String KEY = "23432492384823974289";
+    @Value("${jasypt.encryptor.key}")
+    private String KEY;
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor(){
--- src/main/java/com/usefulNVersatileWeb/usefulWeb/config/JasyptConfigTest.java
+++ src/main/java/com/usefulNVersatileWeb/usefulWeb/config/JasyptConfigTest.java
@@ -2,13 +2,10 @@ package com.usefulNVersatileWeb.usefulWeb.config;
 
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 
 public class JasyptConfigTest {
 
     public static void main(String[] args) {
-        aa();
-
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword("23432492384823974289");
@@ -19,14 +16,7 @@ public class JasyptConfigTest {
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
         encryptor.setConfig(config);
 
-        System.out.println(encryptor.encrypt("jdbc:mysql://localhost:3306/park_web_project_db?autoReconnect=true"));
+//        System.out.println(encryptor.encrypt("jdbc:mysql://localhost:3306/park_web_project_db?autoReconnect=true"));
+//        System.out.println(encryptor.decrypt("VdNr7yG/X/Z1e1bYF8sBgnNWHvY6300uHTvRWtIuGDUvRIgveMdxeN/Z5clZ4CGM7PMqg5qDG9jL56GClC1CvdWM2cFJrk0RmqQRgY5kR5U="));
     }
-
-    @Value("${jasypt.encryptor.bean}")
-    private static String a;
-    public static String aa() {
-        System.out.println(a);
-        return a;
-    }
-
 }
